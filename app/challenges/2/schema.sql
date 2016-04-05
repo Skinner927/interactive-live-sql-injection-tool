e85c81e38fe55b7de84f539ec2a2ffc532109ed4
dskinner <dskinner@lastline.com>
2016-04-05 10:07:25 -0400

    Add challenge 3. Add branding.

--- app/challenges/2/schema.sql
+++ app/challenges/2/schema.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (id integer, username text, password text);
 
 
-INSERT INTO users VALUES (1, 'admin', '32dc710633eb1542f4a28a442b604ad0');
-INSERT INTO users VALUES (2, 'frank', '098f6bcd4621d373cade4e832627b4f6');
+INSERT INTO users VALUES (1, 'frank', '098f6bcd4621d373cade4e832627b4f6');
+INSERT INTO users VALUES (2, 'admin', '32dc710633eb1542f4a28a442b604ad0');
 INSERT INTO users VALUES (3, 'joe', '2ab96390c7dbe3439de74d0c9b0b1767');
---
+++ app/challenges/3/config.js
@@ -0,0 +1,23 @@
+(function(exports) {
+  'use strict';
+
+  var MD5 = new exports.Hashes.MD5;
+
+  exports.config = {
+    title: 'User specific injection - A little harder now',
+    description: 'Bypass the login form by logging in as the user "admin".',
+    beforeQuery: function(params){
+      if(params.password){
+        params.password = MD5.hex(params.password);
+      }
+
+      return params;
+    },
+    afterQuery: function(results, success){
+      if(results.length && results[0].username === 'admin'){
+        success();
+      }
+    }
+  };
+
+})(this);
